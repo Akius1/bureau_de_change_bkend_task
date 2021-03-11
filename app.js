0b36b0fa7cdb02f1ffef88c873940c80b143c2b8
Andrew Urom <andrewurom@gmail.com>
2021-03-11 21:10:21 +0000

    implementd a simple handlebar display

--- app.js
+++ app.js
@@ -5,21 +5,35 @@ const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 const stripe = require('stripe')('sk_test_51I1pGZGItSYZHOzZy6lJMT4QQbmJ2Oj10Q5ZPhhcoYhPgn0zX4IjGlcrpIQsz4PANnTeHpFwneuBCLDQUIGID0cU00CCASV4VU');
 const bodyParser =  require('body-parser');
+const exphbs = require('express-handlebars');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
+///andlebars Middleware
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+
+// view engine setup
+app.set('view engine', 'handlebars');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+//Index Route
+app.get('/', (req, res) =>{
+  res.render('index');
+})
+
+
+
+
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
