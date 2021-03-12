6d3e72583ca69c8704ecda4ae6d0369f42403289
Andrew Urom <andrewurom@gmail.com>
2021-03-12 14:50:21 +0000

    Added success page

--- routes/charge.js
+++ routes/charge.js
@@ -1,11 +1,21 @@
 const express = require('express');
+const { default: Stripe } = require('stripe');
 const router = express.Router();
 
 /* GET home page. */
 router.post('/', (req, res, next) => {
  const amount = 2500;
- console.log(req,body);
- res.send('TEST')
+
+console.log(req.body)
+ stripe.customers.create({
+     email: req.body.stripeEmail,
+     source: req.body.stripeToken
+ }).then(customer => stripe.charges.create({
+     amount,
+     description: 'Bureu de Change',
+     currency: 'usd',
+     customer: customer.id,
+ })).then(charge => res.render('success'));
 });
 
 module.exports = router;
